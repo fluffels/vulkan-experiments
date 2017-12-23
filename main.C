5107a352cb4a9946b6783c3d0a0001aacd7ed1f3
Jan CW Kroeze <thejcwk@gmail.com>
2017-12-23 14:58:34 +0200

    Get GLFW working.

--- main.C
+++ main.C
@@ -1,5 +1,7 @@
 #include <cstdlib>
 
+#include <GLFW/glfw3.h>
+
 #include "easylogging++.h"
 
 INITIALIZE_EASYLOGGINGPP
@@ -7,7 +9,29 @@ INITIALIZE_EASYLOGGINGPP
 int
 main (int argc, char** argv) {
     START_EASYLOGGINGPP(argc, argv);
-    LOG(INFO) << "Hello, world!";
+
+    LOG(INFO) << "Initalizing GLFW...";
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    LOG(INFO) << "Creating window...";
+    auto window = glfwCreateWindow(640, 480, "Vulkan Experiments", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return 2;
+    }
+
+    LOG(INFO) << "Swap to window context...";
+    glfwMakeContextCurrent(window);
+
+    LOG(INFO) << "Entering main loop...";
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
 
