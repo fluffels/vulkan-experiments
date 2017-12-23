8c45d7c4536a3020a35c2c1e764b18ddd271f05d
Jan CW Kroeze <thejcwk@gmail.com>
2017-12-23 15:56:41 +0200

    Some GLFW flags that must be set.

--- main.C
+++ main.C
@@ -15,10 +15,13 @@ main (int argc, char** argv) {
     LOG(INFO) << "Initalizing GLFW...";
     if (!glfwInit()) {
         return 1;
+    } else {
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     }
 
     LOG(INFO) << "Creating window...";
-    auto window = glfwCreateWindow(640, 480, "Vulkan Experiments", NULL, NULL);
+    auto window = glfwCreateWindow(640, 480, "Vulkan Experiments", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return 2;
