0d1e55e2df4f7edea686a204e9c91da45a1ee6e8
Jan CW Kroeze <thejcwk@gmail.com>
2017-12-23 16:10:32 +0200

    Instantiate Vulkan.

--- main.C
+++ main.C
@@ -8,6 +8,8 @@
 
 INITIALIZE_EASYLOGGINGPP
 
+VkInstance instance;
+
 int
 main (int argc, char** argv) {
     START_EASYLOGGINGPP(argc, argv);
@@ -34,12 +36,39 @@ main (int argc, char** argv) {
     LOG(INFO) << "Swap to window context...";
     glfwMakeContextCurrent(window);
 
-    LOG(INFO) << "Entering main loop...";
-    while(!glfwWindowShouldClose(window)) {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vk Experiments";
+    appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(
+            &glfwExtensionCount
+    );
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+    if (result != VK_SUCCESS) {
+        LOG(ERROR) << "Could not instantiate Vulkan.";
+    } else {
+        LOG(INFO) << "Entering main loop...";
+        while(!glfwWindowShouldClose(window)) {
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
     }
 
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
