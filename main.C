94c5b5d85a6d47e47577bf2f7b4bed44e2d83fe5
Jan CW Kroezee <thejcwk@gmail.com>
2017-12-27 12:23:06 +0200

    Render pass.

--- main.C
+++ main.C
@@ -29,6 +29,7 @@ struct Pipeline {
     VkShaderModule vertModule;
     VkShaderModule fragModule;
     VkPipelineLayout layout;
+    VkRenderPass pass;
 };
 Pipeline pipeline = {};
 
@@ -571,6 +572,44 @@ main (int argc, char** argv, char** envp) {
             }
         }
 
+        /* NOTE(jan): Create render passes. */
+        {
+            VkAttachmentDescription colorAttachment = {};
+            colorAttachment.format = swapChain.format.format;
+            colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+            colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+            colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+            colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+            colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+            colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+            colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+            VkAttachmentReference colorAttachmentRef = {};
+            colorAttachmentRef.attachment = 0;
+            colorAttachmentRef.layout =
+                    VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+            VkSubpassDescription subpass = {};
+            subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+            subpass.colorAttachmentCount = 1;
+            subpass.pColorAttachments = &colorAttachmentRef;
+
+            VkRenderPassCreateInfo cf = {};
+            cf.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+            cf.attachmentCount = 1;
+            cf.pAttachments = &colorAttachment;
+            cf.subpassCount = 1;
+            cf.pSubpasses = &subpass;
+
+            VkResult r;
+            r = vkCreateRenderPass(
+                    device, &cf, nullptr, &pipeline.pass
+            );
+            if (r != VK_SUCCESS) {
+                LOG(ERROR) << "Could not create render pass.";
+            }
+        }
+
         /* NOTE(jan): Create pipeline. */
         {
             auto code = readFile("shaders/triangle/vert.spv");
@@ -723,6 +762,7 @@ main (int argc, char** argv, char** envp) {
     }
 
     vkDestroyPipelineLayout(device, pipeline.layout, nullptr);
+    vkDestroyRenderPass(device, pipeline.pass, nullptr);
     for (const auto& v: swapChain.imageViews) {
         vkDestroyImageView(device, v, nullptr);
     }
