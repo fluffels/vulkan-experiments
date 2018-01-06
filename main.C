8bb0ba5836f5698018ea6d1b2f24b89fee2bfe0a
Jan CW Kroezee <thejcwk@gmail.com>
2018-01-06 06:21:02 +0200

    Fix texture coordinate loading.

--- main.C
+++ main.C
@@ -533,7 +533,7 @@ main (int argc, char** argv, char** envp) {
                 };
                 vertex.tex = {
                     attrib.texcoords[2 * index.texcoord_index + 0],
-                    attrib.texcoords[2 * index.texcoord_index + 1]
+                    1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
                 };
                 vertex.color = {1.0f, 1.0f, 1.0f};
                 vertices.push_back(vertex);
