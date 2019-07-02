0fa0ad6576e7dc73adc9d3969d236a5975b7b04e
Jan CW Kroeze <thejcwk@gmail.com>
2019-07-02 22:54:44 +0200

    Build shaders

--- build.bat
+++ build.bat
@@ -2,5 +2,10 @@
 # set LIB=D:\Lib\VulkanSDK\1.1.92.1\Lib\;D:\Lib\glfw-3.3.bin.WIN64\lib-vc2019\;%LIB%
 mkdir build
 pushd build
-cl /std:c++latest /I D:\Lib\glfw-3.3.bin.WIN64\include /I D:\Lib\VulkanSDK\1.1.92.1\Include /I D:\Lib\glm ../main.cpp /link /LIBPATH D:\Lib\VulkanSDK\1.1.92.1\Lib\vulkan-1.lib /LIBPATH D:\Lib\glfw-3.3.bin.WIN64\lib-vc2019\glfw3dll.lib
+cl /std:c++latest /I D:\Lib\glfw-3.3.bin.WIN64\include /I D:\Lib\VulkanSDK\1.1.92.1\Include /I D:\Lib\glm ../main.cpp /link D:\Lib\VulkanSDK\1.1.92.1\Lib\vulkan-1.lib D:\Lib\glfw-3.3.bin.WIN64\lib-vc2019\glfw3dll.lib
+popd
+pushd shaders\triangle
+D:\Lib\VulkanSDK\1.1.92.1\Bin\glslangValidator -V shader.vert
+D:\Lib\VulkanSDK\1.1.92.1\Bin\glslangValidator -V shader.geom
+D:\Lib\VulkanSDK\1.1.92.1\Bin\glslangValidator -V shader.frag
 popd
